93e6863583d81295ead2c0f50e68275d8664539b
Anshu Babu <vilify.king@gmail.com>
2025-01-12 01:41:46 +0530

    PUSH PROD CHANGES

--- src/env/env.prod.ts
+++ src/env/env.prod.ts
@@ -1,7 +1,8 @@
-import ENV_MODEL from "./env.model";
+import ENV_MODEL from './env.model';
 
 export const ENV: ENV_MODEL = {
   isProd: true,
-  AUTH_URL: '',
-  DS_URL: 'http://localhost:PROD',
+  AUTH_URL:
+    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBmOA0Qa9pVnT42f3TOisMqiQpBLvxS-vs',
+  DS_URL: 'https://omfomomo-prod-default-rtdb.firebaseio.com/items',
 };
