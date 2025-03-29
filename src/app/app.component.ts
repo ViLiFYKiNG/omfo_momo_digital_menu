14bb14cc04531b06769ac8da5aa9fffdc04e7420
Anshu Babu <vilify.king@gmail.com>
2025-03-29 23:29:55 +0530

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -8,4 +9,8 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor() {
+    console.log('***');
+  }
+}
